16e3767144872770cb22422aa0368778e6934622
Joe Manthey <jdmanthey@gmail.com>
2021-12-02 15:26:00 -0600

    Update 01_index_reference.sh

--- 01_index_reference.sh
+++ 01_index_reference.sh
@@ -2,10 +2,10 @@
 cd references/
 
 #index with samtools and bwa
-samtools faidx wbnu_genome_NewNames__final_assembly.fasta
-bwa index wbnu_genome_NewNames__final_assembly.fasta
+samtools faidx wbnu.fasta
+bwa index wbnu.fasta
 
 # move up a directory to location of picard jar file
 cd ../
 # make the dict file
-java -jar picard.jar CreateSequenceDictionary R=/home/jmanthey/references/wbnu_genome_NewNames__final_assembly.fasta O=/home/jmanthey/references/wbnu_genome_NewNames__final_assembly.dict
+java -jar picard.jar CreateSequenceDictionary R=/home/jmanthey/references/wbnu.fasta O=/home/jmanthey/references/wbnu.dict
